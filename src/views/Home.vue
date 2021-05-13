0cc7307e79ca435b7839ced96bf7fe9f36238adb
Rodion Sibov <rodionsibov@gmail.com>
2021-05-13 16:52:14 +0300

    Composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click Me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  setup() {
+    
+    console.log(this);
+
+    const p = ref(null);
+
+    let name = "Ros";
+    let age = 30;
+
+    const handleClick = () => {
+      console.log(p.value);
+      p.value.classList.add('test')
+    };
+
+    return {
+      name,
+      age,
+      handleClick,
+      p,
+    };
+  },
+};
 </script>
